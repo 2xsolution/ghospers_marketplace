1c523081ba0694e485744fd139453ddf099930d2
Prince Bansal <1726250.cse.coe@cgc.edu.in>
2022-03-29 14:15:05 +0530

    Properties added

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,90 +3,88 @@ import { NavLink, Link } from "react-router-dom";
 import logo from "../assets/img/logo.6eaa2fdb.png";
 import "./header.css";
 
-import {
-	loadWeb3,
-	connectWallet,
-	getCurrentWallet,
-} from "../core/web3";
+import { loadWeb3, connectWallet, getCurrentWallet } from "../core/web3";
 
 const Header = ({ setShowModal }) => {
-	const [navActive, isnavActive] = useState(false);
-	const [curWallet, setCurWallet] = useState("");
+  const [navActive, isnavActive] = useState(false);
+  const [curWallet, setCurWallet] = useState("");
 
-	const onConnectWallet = async () => {
-		if (curWallet != "") {
-			return;
-		}
-		await loadWeb3();
-		let res = await connectWallet();
-		setCurWallet(res.address);
-	}
+  const onConnectWallet = async () => {
+    if (curWallet != "") {
+      return;
+    }
+    await loadWeb3();
+    let res = await connectWallet();
+    setCurWallet(res.address);
+  };
 
-	const openModal = (e) => {
-		console.log("hwllo");
-		e.preventDefault();
-		// setShowModal((prev) => !prev);
+  const openModal = (e) => {
+    console.log("hwllo");
+    e.preventDefault();
+    // setShowModal((prev) => !prev);
 
-		onConnectWallet();
-	};
+    onConnectWallet();
+  };
 
-	const getWallet = async () => {
-		let res = await getCurrentWallet();
-		if (res.success) {
-			setCurWallet(res.account);
-		}
-	}
+  const getWallet = async () => {
+    let res = await getCurrentWallet();
+    if (res.success) {
+      setCurWallet(res.account);
+    }
+  };
 
-	useEffect(() => {
-		getWallet();
-	});
+  useEffect(() => {
+    getWallet();
+  });
 
-	return (
-		<header>
-			<div className="container">
-				<Link to="/">
-					<div className="logo">
-						<img src={logo} alt="logo" />
-					</div>
-				</Link>
-				<div className={navActive ? "links nav-active" : "links"}>
-					<ul className="navigations">
-						<li>
-							<Link to="https://ghospers.com/" activeclassname="active">
-								Home
-							</Link>
-						</li>
-						<li>
-							<NavLink to="/" activeclassname="active">
-								Official
-							</NavLink>
-						</li>
-						<li>
-							<Link to="https://staking.ghospers.com/" activeclassname="active">
-								Staking
-							</Link>
-						</li>
-						{/* <li>
+  return (
+    <header>
+      <div className="container">
+        <Link to="/">
+          <div className="logo">
+            <img src={logo} alt="logo" />
+          </div>
+        </Link>
+        <div className={navActive ? "links nav-active" : "links"}>
+          <ul className="navigations">
+            <li>
+              <Link to="https://ghospers.com/" activeclassname="active">
+                Home
+              </Link>
+            </li>
+            <li>
+              <NavLink to="/" activeclassname="active">
+                Market Place
+              </NavLink>
+            </li>
+            <li>
+              <Link to="https://staking.ghospers.com/" activeclassname="active">
+                Staking
+              </Link>
+            </li>
+            {/* <li>
 							<NavLink to="/contact" activeclassname="active">
 								Contact Us
 							</NavLink>
 						</li> */}
-					</ul>
-					<div className="nav-btn">
-						<a href="/" onClick={openModal}>
-							{curWallet == "" ? "Connect Wallet" : curWallet.slice(0, 5) + "..." + curWallet.slice(-4)}
-						</a>
-						<a href="/">Login</a>
-					</div>
-				</div>
-				<div className="hamburger" onClick={() => isnavActive(!navActive)}>
-					<div className="line1"></div>
-					<div className="line2"></div>
-					<div className="line3"></div>
-				</div>
-			</div>
-		</header>
-	);
+          </ul>
+          <div className="nav-btn">
+            <a href="/" onClick={openModal}>
+              {curWallet == ""
+                ? "Connect Wallet"
+                : curWallet.slice(0, 5) + "..." + curWallet.slice(-4)}
+            </a>
+            <a href="/">Login</a>
+          </div>
+        </div>
+        <div className="hamburger" onClick={() => isnavActive(!navActive)}>
+          <div className="line1"></div>
+          <div className="line2"></div>
+          <div className="line3"></div>
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
