e56f20e5ce664176627d27ec994c6397f1ba10a9
Prince Bansal <1726250.cse.coe@cgc.edu.in>
2022-04-16 14:23:39 +0530

    border fixed

--- src/utils/Utils.js
+++ src/utils/Utils.js
@@ -1,14 +1,8 @@
 import { toast } from "react-hot-toast";
 
-<<<<<<< HEAD
 // export const BASEURL = "http://localhost:4000";
 // export const BASEURL = "https://nftmintbackend.herokuapp.com";
 export const BASEURL = "https://ghospers.herokuapp.com";
-=======
-//export const BASEURL = "http://localhost:4000";
-// export const BASEURL = "https://nftmintbackend.herokuapp.com";
- export const BASEURL = "https://ghospers.herokuapp.com";
->>>>>>> 70067d75dc71729436de6ef2d39c4fe80425bbb7
 
 export const Success = (message) => toast.success(message);
 export const Error = (message) => toast.error(message);
