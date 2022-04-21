cf9e74d802702545138b9ef00c971ac31ddcec6b
coders003 <94458357+coders003@users.noreply.github.com>
2022-04-22 04:56:37 +0530

    url

--- src/utils/Utils.js
+++ src/utils/Utils.js
@@ -1,8 +1,8 @@
 import { toast } from "react-hot-toast";
 
-export const BASEURL = "http://localhost:4000";
+// export const BASEURL = "http://localhost:4000";
 // export const BASEURL = "https://nftmintbackend.herokuapp.com";
-// export const BASEURL = "https://ghospers.herokuapp.com";
+export const BASEURL = "https://ghospers.herokuapp.com";
 
 export const Success = (message) => toast.success(message);
 export const Error = (message) => toast.error(message);
